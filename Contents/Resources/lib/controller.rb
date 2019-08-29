52297aa1e9a420d2d40ad586d9628ba4b3247bfe
Roben Kleene <contact@robenkleene.com>
2019-08-29 18:21:06 -0400

    Add ERB methods

--- Contents/Resources/lib/controller.rb
+++ Contents/Resources/lib/controller.rb
@@ -1,5 +1,6 @@
 require 'redcarpet'
 require_relative 'view'
+require 'erb'
 
 module Repla
   module Markdown
@@ -15,6 +16,17 @@ module Repla
 
       def markdown=(markdown)
         @view.html = @renderer.render(markdown)
+      end
+
+      def load_erb_from_path(path)
+        erb = File.new(path).read
+        load_erb(erb)
+      end
+
+      def load_erb(erb)
+        template = ERB.new(erb, nil, '-')
+        html = template.result(binding)
+        load_html(html)
       end
     end
   end
