8c5b0e793f004e4c7066f863620c2645ccba790c
Roben Kleene <contact@robenkleene.com>
2020-01-17 15:13:51 -0500

    Update API

--- Contents/Resources/lib/view.rb
+++ Contents/Resources/lib/view.rb
@@ -10,7 +10,7 @@ module Repla
         super()
         @html = html
         @filename = filename
-        self.base_url_path = File.expand_path(BASE_DIRECTORY)
+        self.root_access_directory_path = File.expand_path(BASE_DIRECTORY)
         load_erb_from_path(VIEW_TEMPLATE)
       end
 
