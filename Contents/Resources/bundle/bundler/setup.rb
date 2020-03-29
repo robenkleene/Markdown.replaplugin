071be9c2d4a1ca03162c8352b7ca1e5965847b5b
Roben Kleene <contact@robenkleene.com>
2020-03-29 16:20:05 -0400

    Run bundle update

--- Contents/Resources/bundle/bundler/setup.rb
+++ Contents/Resources/bundle/bundler/setup.rb
@@ -4,8 +4,8 @@ ruby_engine = defined?(RUBY_ENGINE) ? RUBY_ENGINE : 'ruby'
 ruby_version = RbConfig::CONFIG["ruby_version"]
 path = File.expand_path('..', __FILE__)
 $:.unshift "#{path}/"
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/extensions/x86_64-darwin-18/2.4.0-static/ffi-1.12.1"
-$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/ffi-1.12.1/lib"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/extensions/x86_64-darwin-18/2.4.0-static/ffi-1.12.2"
+$:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/ffi-1.12.2/lib"
 $:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/flt-1.5.2/lib"
 $:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/rb-fsevent-0.10.3/lib"
 $:.unshift "#{path}/../#{ruby_engine}/#{ruby_version}/gems/rb-inotify-0.10.1/lib"
