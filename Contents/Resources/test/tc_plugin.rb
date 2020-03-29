6d63e556509b1504c60a6bc16eba04666d9e09a6
Roben Kleene <contact@robenkleene.com>
2020-03-29 17:25:38 -0400

    Work on plugin test

--- Contents/Resources/test/tc_plugin.rb
+++ Contents/Resources/test/tc_plugin.rb
@@ -1,18 +1,17 @@
 #!/usr/bin/env ruby
 
+require 'minitest/autorun'
+
 require_relative 'lib/test_constants'
 require_relative '../bundle/bundler/setup'
-
+require 'repla/test'
+require Repla::Test::REPLA_FILE
 require Repla::Test::HELPER_FILE
 
 # Test plugin
 class TestPlugin < Minitest::Test
   def setup
     Repla.load_plugin(TEST_PLUGIN_PATH)
-  end
-
-  def teardown
-    window.close
   end
 
   def test_load_markdown_file
@@ -28,8 +27,13 @@ class TestPlugin < Minitest::Test
 
     window_id = Repla.window_id_for_plugin(TEST_PLUGIN_NAME)
     window = Repla::Window.new(window_id)
-
-    header = window.do_javascript(TEST_H1_JAVASCRIPT)
+    header = nil
+    Repla::Test.block_until do
+      header = window.do_javascript(TEST_H1_JAVASCRIPT)
+      header == TEST_MARKDOWN_HEADER
+    end
     assert_equal(header, TEST_MARKDOWN_HEADER)
+
+    window.close
   end
 end
